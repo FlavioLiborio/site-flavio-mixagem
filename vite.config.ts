a29fe2a2d7c6f548bbd86c04be665a8c33204bf2
FlavioLiborio <contato@flavioliborio.com.br>
2025-12-10 16:33:58 -0300

    Solução Final: Reconstrução com versões estáveis

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// ATUALIZE VITE.CONFIG.TS SE A REINSTALAÇÃO FALHAR
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './', 
   
@@ -10,9 +10,5 @@ export default defineConfig({
     cssCodeSplit: true, 
   },
   
-  experimental: {
-    cleanUrls: true,
-  },
-  
   plugins: [react()],
-})
+});
