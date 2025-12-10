7e3a97001f2ecd7081c255fb1d669cba1974714c
FlavioLiborio <contato@flavioliborio.com.br>
2025-12-10 15:21:32 -0300

    Corrigido o build do CSS no vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-base: 'https://site-flavio-mixagem.vercel.app/',
+  base: './', 
+  
+  build: {
+    assetsDir: 'assets',
+    cssCodeSplit: true, 
+  },
+  
   plugins: [react()],
 })
